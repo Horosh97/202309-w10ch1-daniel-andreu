810943ec35f751792189a38010bf004b6c31d537
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-13 21:31:13 +0100

    Add get /:thingId functionality

--- src/features/things/routers/thingsRouter.ts
+++ src/features/things/routers/thingsRouter.ts
@@ -8,4 +8,17 @@ thingsRouter.get("/", (_req, res) => {
   res.json({ things });
 });
 
+thingsRouter.get("/:thingId", (req, res) => {
+  const { thingId } = req.params;
+
+  const thing = things.find((thing) => thing.id === +thingId);
+
+  if (!thing) {
+    res.status(404).json({ error: "Thing not found" });
+    return;
+  }
+
+  res.status(200).json({ thing });
+});
+
 export default thingsRouter;
