f7286e799cea72a9016cd67ee943b547ca0f9772
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-13 21:29:48 +0100

    Add get things functionality

--- src/features/things/routers/thingsRouter.ts
+++ src/features/things/routers/thingsRouter.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import things from "../data/things.js";
 
 const thingsRouter = Router();
+
+thingsRouter.get("/", (_req, res) => {
+  res.status(200);
+  res.json({ things });
+});
 
 export default thingsRouter;
