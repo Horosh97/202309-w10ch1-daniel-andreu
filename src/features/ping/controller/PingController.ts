fdbea935ecc1002b11ecb9f789ecb3f56b1ce8da
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-15 15:20:43 +0100

    Add PingController tests

--- src/features/ping/controller/PingController.ts
+++ src/features/ping/controller/PingController.ts
@@ -2,7 +2,7 @@ import { type Request, type Response } from "express";
 
 class PingController {
   getPong(_req: Request, res: Response) {
-    res.status(200).json({ message: "pong" });
+    res.status(200).json({ message: "🏓" });
   }
 }
 export default PingController;
