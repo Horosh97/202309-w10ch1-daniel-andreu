ae3b9dca6522c77863e5e74a86d7a51ac4ba9b6e
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-15 11:50:32 +0100

    Add ping router and needed imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import morgan from "morgan";
 import app from "./app.js";
 import thingsRouter from "./features/things/routers/thingsRouter.js";
+import express from "express";
+import pingRouter from "./features/ping/router/pingRouter.js";
 
+app.use(express.json());
 app.use(morgan("dev"));
 
+app.use("/", pingRouter);
 app.use("/things", thingsRouter);
