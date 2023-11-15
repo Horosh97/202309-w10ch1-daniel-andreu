8d7d4e10d031b59cf3eafe0decfb4066c9f8556d
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-15 13:40:44 +0100

    Add middlewareError

--- src/features/ping/controller/PingController.ts
+++ src/features/ping/controller/PingController.ts
@@ -2,7 +2,7 @@ import { type Request, type Response } from "express";
 
 class PingController {
   getPong(_req: Request, res: Response) {
-    res.status(200).json({ message: "ping" });
+    res.status(200).json({ message: "pong" });
   }
 }
 export default PingController;
